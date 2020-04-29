c3941b0817bc6298ea20f39194f0ee3d3a3f36e7
GhostX <egamovkhurshed29@gmail.com>
2020-04-29 14:24:38 +0500

    Add some propertions in class Client and some function if crud has been done

--- Client.cs
+++ Client.cs
@@ -2,6 +2,11 @@ namespace Client
 {
     public class Client
     {
-
+        public int Id { get; set; } 
+        public decimal Balance { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal LastBalance { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Client
 {
     class Program
     {
+        static public List<Client> clients = new List<Client>();
         static void Main(string[] args)
         {
+            Thread insert = new Thread(Insert);
+            insert.Start();
+            Console.ReadKey();
+        }
+        static void SelectAll()
+        {
+            Console.WriteLine("Id FirstName LastName Age Balance");
+            foreach (var client in clients)
+            {
+                Console.WriteLine($"{client.Id} {client.FirstName} {client.LastName} {client.Age} {client.Balance}");
+            }
+        }
+        static void Update()
+        {
+            Console.WriteLine("Reenter FirstName:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Reenter LastName:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Reenter Age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Reenter Balance:");
+            decimal balance = decimal.Parse(Console.ReadLine());
+        }
+        static void Insert()
+        {
+            Console.WriteLine("Enter FirstName:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Balance:");
+            decimal balance = decimal.Parse(Console.ReadLine());
+            int id = CheckId();
+            clients.Add(new Client()
+            {
+                Id = id,
+                Age = age,
+                Balance = balance,
+                FirstName = firstName,
+                LastName = lastName,
+                LastBalance = balance,
+            });
+        }
+        static int CheckId()
+        {
+            int id = 1;
+            try
+            {
+                id = clients[clients.Count - 1].Id + 1;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return id;
         }
     }
 }
