62e341373b41bb233f2364e3eea1bc7a1c92b085
GhostX <egamovkhurshed29@gmail.com>
2020-04-29 19:24:14 +0500

    Added some functions

--- Program.cs
+++ Program.cs
@@ -7,12 +7,61 @@ namespace Client
     class Program
     {
         static public List<Client> clients = new List<Client>();
+        static int id = 0;
         static void Main(string[] args)
         {
-            Thread insert = new Thread(Insert);
-            insert.Start();
-            Console.ReadKey();
+            Thread func = new Thread(Functions);
+            func.Start();
         }
+        static void Functions()
+        {
+            bool start = true;
+            Thread insert = new Thread(Insert);
+            Thread selectAll = new Thread(SelectAll);
+            Thread update = new Thread(Update);
+            Thread delete = new Thread(Delete);
+            while (start)
+            {
+                selectAll.Start();
+                Console.WriteLine("1-for select one.\n2-for insert one");
+                int choose = int.Parse(Console.ReadLine());
+                switch (choose)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Enter client Id from upper:");
+                            id = int.Parse(Console.ReadLine());
+                            Console.WriteLine("1-for delete\n2- for update\n3-to go to the main menu");
+                            int chs = int.Parse(Console.ReadLine());
+                            switch (chs)
+                            {
+                                case 1:
+                                    {
+                                        Delete();
+                                    }
+                                    break;
+                                case 2:
+                                    {
+                                        Update();
+                                    }
+                                    break;
+                            }
+                        }
+                        break;
+                    case 2:
+                        {
+                            Insert();
+                        }
+                        break;
+                }
+            }
+        }
+
+        static void Delete()
+        {
+
+        }
+
         static void SelectAll()
         {
             Console.WriteLine("Id FirstName LastName Age Balance");
@@ -31,6 +80,20 @@ namespace Client
             int age = int.Parse(Console.ReadLine());
             Console.WriteLine("Reenter Balance:");
             decimal balance = decimal.Parse(Console.ReadLine());
+            int i = 0;
+            foreach (var client in clients)
+            {
+                if (client.Id == id)
+                {
+                    break;
+                }
+                i++;
+            }
+            clients[i].FirstName = firstName;
+            clients[i].LastName = lastName;
+            clients[i].LastBalance = clients[i].Balance;
+            clients[i].Age = age;
+            clients[i].Balance = balance;
         }
         static void Insert()
         {
